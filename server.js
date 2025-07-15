9f30796f1c11ac50cbf24801d3d7f09db55ef501
kosei nakazawa <scottie20001025@gmail.com>
2025-07-15 09:41:53 +0900

    Update server.js

--- server.js
+++ server.js
@@ -6,10 +6,6 @@ const port = process.env.PORT || 3000;
 // 静的ファイルを public フォルダから提供
 app.use(express.static(path.join(__dirname, 'public')));
 
-// ルートアクセス時に index.html を返す
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
